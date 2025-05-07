d936ec9f79e5452a2e9be813957762fb52b6abdf
Kelvin <qamaukelvin@gmail.com>
2025-05-07 12:08:53 +0300

    loader

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+    document.getElementById("loader-wrapper").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  });
+  
